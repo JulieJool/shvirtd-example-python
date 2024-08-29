e27c32ed8a39ad609774cf5c1d88d829738b5239
JulieJool <150677323+JulieJool@users.noreply.github.com>
2024-08-29 18:24:09 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+mkdir opt
+cd opt
+git init
+git config user.name juliejool
+git config user.email julieteplov@gmail.com
+
+# the repositry is public so we're gonna connect via HTTPS (without creating SSH keys).
+git remote add origin https://github.com/JulieJool/shvirtd-example-python.git
+git checkout -b main
+git pull origin main
+docker compose up -d
+
 
